895cc8d1ff1f1c5b33d508dd7887e080020f3e97
zhangyudong12 <61321464+zhangyudong12@users.noreply.github.com>
2021-07-06 11:20:02 -0400

    Add files via upload

--- test/peeringdb_test.py
+++ test/peeringdb_test.py
@@ -2,9 +2,14 @@ import unittest
 import peeringdb as pb
 
 class testpeeringdb(unittest.TestCase):
+    def test_lookupNet(self):
+        expected = "Twitch"
+        result = pb.lookupNet(1956)
+        self.assertEqual(result, expected)
+
     def test_getix(self):
         asn = 49909
-        expected1 = ["Balcan-IX","RoNIX","InterLAN"]
+        expected1 = ["Balcan-IX","InterLAN","RoNIX"]
         expected2 = 5000
         result1, result2 = pb.getIX(asn)
         self.assertEqual(result1, expected1)
@@ -14,7 +19,11 @@ class testpeeringdb(unittest.TestCase):
     def test_findpeerings(self):
         asn = 49909
         ix_name = "Balcan-IX"
-        expected = ['Ad Net Market Media SA', 'BALCAN-IX Route Servers', 'BALCAN-IX Route Servers', 'Cloudflare', 'DOTRO Telecom', 'Distinct New Media', 'Expansion Computers', 'Facebook Inc', 'Facebook Inc', 'Hurricane Electric', 'I.S. Centrul de Telecomunicatii Speciale', 'IPTP Networks', 'Infinity Telecom SRL', 'M247', 'M247', 'Microsoft', 'Microsoft', 'Moldtelecom', 'NAV Communications', 'Neotel', 'Neterra Ltd.', 'Pixel View SRL', 'Prime Telecom', 'RoEduNet', 'Serbia BroadBand', 'Serbian Open eXchange', 'TENNET TELECOM', 'TV SAT 2002', 'TeenTelecom', 'Telehouse', 'Telehouse', 'Telekom Romania Communications S.A.', 'Türk Telekom International', 'VOXILITY', 'Valve Corporation', 'Valve Corporation', 'Verizon Digital Media Services (EdgeCast Networks)', 'Yahoo!']
+        expected = [  "AS_10310_Yahoo!","AS_13004_Serbian Open eXchange","AS_13335_Cloudflare","AS_15133_Verizon Digital Media Services (EdgeCast Networks)","AS_24745_BALCAN-IX Route Servers","AS_24745_BALCAN-IX Route Servers","AS_2614_RoEduNet",
+        "AS_31042_Serbia BroadBand","AS_3223_VOXILITY","AS_32590_Valve Corporation","AS_32590_Valve Corporation","AS_32934_Facebook Inc","AS_32934_Facebook Inc","AS_34224_Neterra Ltd.","AS_34304_TeenTelecom",
+        "AS_34772_Neotel","AS_35711_Expansion Computers","AS_39279_I.S. Centrul de Telecomunicatii Speciale","AS_39543_TENNET TELECOM","AS_39737_Prime Telecom","AS_41095_IPTP Networks","AS_41496_TV SAT 2002",
+        "AS_43376_DOTRO Telecom","AS_48067_Distinct New Media","AS_50244_Pixel View SRL","AS_5541_Ad Net Market Media SA","AS_56654_Infinity Telecom SRL","AS_57344_Telehouse","AS_57344_Telehouse","AS_6663_T\u00fcrk Telekom International",
+        "AS_6718_NAV Communications","AS_6939_Hurricane Electric","AS_8075_Microsoft","AS_8075_Microsoft","AS_8926_Moldtelecom","AS_9009_M247","AS_9009_M247","AS_9050_Telekom Romania Communications S.A."]
         result = pb.findPeerings(ix_name,asn)
         self.assertEqual(result, expected)
 
