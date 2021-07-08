bb0f2e1e3a12ac96a7cba12a967d304bba9cee7d
zhangyudong12 <61321464+zhangyudong12@users.noreply.github.com>
2021-07-08 10:29:28 -0400

    refactoring

--- peeringdb_ms.py
+++ peeringdb_ms.py
@@ -4,11 +4,12 @@
 
 # Purpose: Generate a peeringDB state report for a specific network
 
-# Multi-threading
+# Multi-threading and refactoring
 
 import requests
 import json
 import sys
+import os
 import time
 from datetime import timedelta
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -77,7 +78,6 @@ def getIX(search):
     return ix_list,fac_list,ix_speed
 
 
-# def findPeerings(search, asn):
 # input:IX name and asn, output:sorted network list exclude the input asn
 def findPeerings(search, asn):
     # match exact name
@@ -109,37 +109,62 @@ def findPeerings(search, asn):
     return net_list
 
 
-def print_report(title,input_dict,flag):
+def print_report(title,input,flag):
     print("\n")
     print('~' * 79)
     print(title)
     print('~' * 79)
-    if flag == 'join':
-        for key,value in input_dict.items():
-            sub_total_peers = len(value)
-            print(f"\nIX: {key}   Peering's : {sub_total_peers}")
-            print('\n'.join(value))
+    if type(input) == dict:
+        if flag == 'join':
+            for key,value in input.items():
+                sub_total_peers = len(value)
+                print(f"\nIX: {key}   Peering's : {sub_total_peers}")
+                print('\n'.join(value))
+        else:
+            for key,value in input.items():
+                print (key,value)
+    elif type(input) == list:
+        input_set = set(input)
+        item_count = {item: input.count(item) for item in input_set}
+        for item, count in item_count.items():
+            if count > 1:
+                print(f"{item} : #number of connections  {count}")
+            else:
+                continue
     else:
-        for key,value in input_dict.items():
-            print (key,value)
-
+        print("Wrong input")
+        exit(1)
 
 def export_file(file_name,contents):
-    with open(file_name, 'w') as json_file:
+    newdir = 'test_log'
+    try:
+        os.mkdir(newdir)
+    except OSError as error:
+        # FileExistsError is error # 17
+        if error.errno == 17:
+            print('Directory', newdir, 'already exists.')
+        else:
+            # re-raise the exception if some other error occurred.
+            raise
+    filename_full = os.path.join(newdir, file_name)
+    with open(filename_full, 'w') as json_file:
         json.dump(contents, json_file, indent=2)
     json_file.close()
 
 
 def main():
+    # Process input variable
     asn = int(get_input('Enter Network AS number: '))
     print_to_file = get_input('Print the output to file y/n ?: ')
     if print_to_file.upper() == 'Y':
         orig_stdout = sys.stdout
-        f = open('script_cmd_output.txt', 'w')
+        output_filename = 'script_cmd_output_' + str(asn) + '.txt'
+        f = open(output_filename, 'w')
         sys.stdout = f
 
     start_time = time.time()
-    asn_report = dict()
+
+    # Process ASN query data
     ix_list_asn, fac_list_asn, total_agg_speed_m = getIX(asn)
     ix_set_asn = set(ix_list_asn)
     total_agg_speed = total_agg_speed_m/1000
@@ -147,16 +172,19 @@ def main():
     total_fac = len(fac_list_asn)
     ix_set_asn_str = '\n'.join(ix_set_asn)
     fac_list_asn_str = '\n'.join(fac_list_asn)
+
     print('~' * 79)
     print(f"Network with ASN = {asn} exists in {total_ix} Public Exchange Points and {total_fac} Private Peering Facilities")
     print(f"\nPublic IXs:\n{ix_set_asn_str}")
     if total_fac != 0:
         print(f"\nPrivate FAs:\n{fac_list_asn_str}")
 
+    # sort the output data to dict
     all_peers = dict()
     for ix in ix_set_asn:
         all_peers[ix] = findPeerings(ix,asn)
 
+    # deduplication
     mergedlist = []
     for ix in all_peers:
         mergedlist.extend(all_peers[ix])
@@ -164,32 +192,22 @@ def main():
     total_peers = len(mergedlist)
     mergedset = set(mergedlist)
     total_organizations = len(mergedset)
+
     asn_report = {"ASN":asn,"Total_agg_speed(Gbps)":total_agg_speed,"Total_ix":total_ix,"Total_peers":total_peers,"Total_organizations":total_organizations}
 
+    # print report
     print_report("ASN Network Executive Summary :", asn_report, 'na')
     print_report("ASN Peering's List per Public IX :", all_peers, 'join')
+    print_report("The additional information for IXs where the ASN has more connections: ",ix_list_asn,'na')
+    print_report("The additional information for peering's with more than one connection points:: ",mergedlist,'na')
 
-    print("\n\nThe additional information for IXs where the ASN has more connections: ")
-    print('~' * 79)
-    net_count = {net: ix_list_asn.count(net) for net in ix_set_asn}
-    for net, count in net_count.items():
-        if count > 1:
-            print(f"{net} : #number of network connections  {count}")
-        else:
-            continue
+    # export report to file
+    filename1 = 'asn_report_' + str(asn) + '.json'
+    filename2 = 'ix_net_report_' + str(asn) + '.json'
+    export_file(filename1, asn_report)
+    export_file(filename2, all_peers)
 
-    print("\n\nThe additional information for peering's with more than one connection points: ")
-    print('~' * 79)
-    peer_count = {org: mergedlist.count(org) for org in mergedset}
-    for peer, count in peer_count.items():
-        if count > 1:
-            print(f"{peer} : #number of connection points  {count}")
-        else:
-            continue
-
-    export_file('asn_report.json', asn_report)
-    export_file('ix_net_report.json', all_peers)
-
+    # calculate and display script running time
     elapsed_time_secs = time.time() - start_time
     elapsed_time = timedelta(seconds=round(elapsed_time_secs))
     print(f"\n\n\nScript execution took: {elapsed_time}")
@@ -203,17 +221,16 @@ if __name__ == "__main__":
 
 """ Note:
 
-1 The specific Org might have more than one peer connections in one or more IXs.
-The additional information provides the Orgs list and nummber of connections
+ 1 The additional information shows the IXs or Orgs list with redundnact connections
 
-2 The output json file will be exported to mysql database used for web app
+ 2 The output json file will be exported to mysql database used for web app
 
-3 The peeringdb output is nested dictionary. some data need to be retrived from inner dict()
+ 3 The peeringdb output is a nested dictionary. some data needs to be retrived from inner dict()
 
-4 Ocationally the peeringDB API server doesn't respond to https request, that will interupt the running script.
-this will occurs likely when the network being queried has widely distributed in >50 IXs
+ 4 Ocationally the peeringDB API server doesn't respond to https request, that will interupt the running script.
+   It likely occur when the querring network presents in >50 IXs
 
-5 Use ASN = 49909/49902/49904 or ASN = 852 for quick test as all that only exist in less than 3x IXs.
+ 5 Use ASN = 49909/49902/49904 or ASN = 852 for quick test as all that only exist in less than 3x IXs.
 
 
 """
